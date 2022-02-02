787ee6ee22d644e5c22caefedf5dcc72d1d902f3
Quan <quantrung286@gmail.com>
2022-02-02 15:05:39 +0700

    up

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -17,7 +17,7 @@ export const Result = () => {
 				getResults(`${location.pathname}/q=${searchTerm}&num=40`)
 			}
 		}
-	}, [ searchTerm, location.pathname])
+	}, [searchTerm, location.pathname])
 
 	if (isLoading) return <Loading />
 
@@ -40,37 +40,45 @@ export const Result = () => {
 		case '/images':
 			return (
 				<div className='flex flex-wrap justify-center items-center'>
-					{results.map(
-						({ image, link: { href, title } }, index) => (
-							<a
-								key={index}
-								href={href}
-								className='sm:p-3 p-5'
-								target='_blank'
-								rel='noreferrer'
-							>
-								<img src={image.src} alt={title} loading='lazy' />
-								<p className='w-36 break-words text-sm mt-2'>{title}</p>
-							</a>
-						)
-					)}
+					{results.map(({ image, link: { href, title } }, index) => (
+						<a
+							key={index}
+							href={href}
+							className='sm:p-3 p-5'
+							target='_blank'
+							rel='noreferrer'
+						>
+							<img src={image.src} alt={title} loading='lazy' />
+							<p className='w-36 break-words text-sm mt-2'>{title}</p>
+						</a>
+					))}
 				</div>
 			)
 		case '/news':
 			return (
-				<div className='flex flex-wrap justify-between space-y-6 sm:px-56 items-center'>
-					{results.map(({ links, id, source, title }) => (
-						<div key={id} className='md:w-2/5 w-full'>
-							<a href={links[0].href} target='_blank' rel='noreferrer' className=''>
-								<p className='text-lg dark:text-blue-300 text-blue-700 hover:underline'>
+				<div className='sm:px-56 flex flex-wrap justify-between items-center space-y-6'>
+					{results.map(({ id, links, source, title }) => (
+						<div key={id} className='md:w-2/5 w-full '>
+							<a
+								href={links[0].href}
+								target='_blank'
+								rel='noreferrer '
+								className='hover:underline '
+							>
+								<p className='text-lg dark:text-blue-300 text-blue-700'>
 									{title}
 								</p>
-								<div className='flex gap-4'>
-								<p href={source.href} target='_blank' rel='noreferrer' >
-								{source.href}
-								</p>
-								</div>
 							</a>
+							<div className='flex gap-4'>
+								<a
+									href={source.href}
+									target='_blank'
+									rel='noreferrer'
+									className='hover:underline hover:text-blue-300'
+								>
+									{source.href}
+								</a>
+							</div>
 						</div>
 					))}
 				</div>
@@ -80,7 +88,12 @@ export const Result = () => {
 				<div className='flex flex-wrap'>
 					{results.map((video, index) => (
 						<div key={index} className='p-2'>
-							<ReactPlayer url={video.additional_links?.[0].href} controls width='355px' height='200px' />
+							<ReactPlayer
+								url={video.additional_links[0].href}
+								controls
+								width='355px'
+								height='200px'
+							/>
 						</div>
 					))}
 				</div>
