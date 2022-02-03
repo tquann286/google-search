769d90c493574670f7da8fd771dd93f499421623
Quan <quantrung286@gmail.com>
2022-02-03 17:15:27 +0700

    up

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -25,7 +25,7 @@ export const Result = () => {
 		case '/search':
 			return (
 				<div className='flex flex-wrap justify-between space-y-6 sm:px-56'>
-					{results.map(({ link, title }, index) => (
+					{results?.map(({ link, title }, index) => (
 						<div key={index} className='md:w-2/5 w-full'>
 							<a href={link} target='_blank' rel='noreferrer'>
 								<p className='text-sm'>{link}</p>
